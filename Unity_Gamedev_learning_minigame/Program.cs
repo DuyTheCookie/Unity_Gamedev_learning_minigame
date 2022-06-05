df5f24699e7295038b23bba6dfcbcae09072b3bf
mian17 <nightknight011@gmail.com>
2022-06-05 14:55:35 +0700

    Add project files.

---
+++ Unity_Gamedev_learning_minigame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Unity_Gamedev_learning_minigame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
